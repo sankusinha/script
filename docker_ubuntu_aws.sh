006728ab3ec2347731128567ecafda89cd785d66
Sanku Sinha <sanku.sinha@cis.ntt.com>
2019-08-30 17:29:01 +0530

    docker_ubuntu_aws modified

--- docker_ubuntu_aws.sh
+++ docker_ubuntu_aws.sh
@@ -2,3 +2,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
+sudo usermod -aG docker ${USER}
